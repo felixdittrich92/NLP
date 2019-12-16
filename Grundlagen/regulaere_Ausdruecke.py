9cc75c8c9fddca82d211fa6349a3db087f7872fa
Felix <you@example.com>
2019-12-16 12:35:29 +0100

    step

--- Grundlagen/regulaere_Ausdruecke.py
+++ Grundlagen/regulaere_Ausdruecke.py
@@ -27,3 +27,32 @@ print(phone.group())
 pattern = r'\d{3}-\d{3}-\d{4}'
 match = re.search(pattern, text)
 print(match)
+
+# gruppieren
+print(phone.group())
+pattern = r'(\d{3})-(\d{3})-(\d{4})'
+phone = re.search(pattern, text)
+print(phone.group(2))
+
+# ODER Suche
+result = re.search(r'Mann|Frau', "Dieser Mann war hier.")
+print(result)
+
+# nach Endung suchen
+result = re.findall(r'.und', "Der Hund mit dem großen Mund war gesund.")
+print(result)
+# Leerzeichen nicht beachten // siehe Doku für weitere "Kürzungen"
+result = re.findall(r'\S+und', "Der Hund mit dem großen Mund war gesund.")
+print(result)
+# Ausschluss von Zeichen
+text = "Es sind 3 Nummern 34 in 5 diesem Satz."
+result = re.findall(r'[^\d]+', text)
+print(result)
+# Satzzeichen entfernen
+text = "Dies ist ein String! Aber er hat Satzzeichen. Wie können wir sie entfernen ?"
+result = re.findall(r'[^!.?]+', text)
+print(result)
+# gruppieren direkt
+text = "Finde nur die mit Binde-Strich getrennten Wörter in diesem Satz. Du weißt aber nicht, wie um-fassend sie sind."
+result = re.findall(r'[\w]+-[\w]+', text)
+print(result)
