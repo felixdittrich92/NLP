d2a835e7e2a6b2c7ebf91882192dcd9b1376cb03
Felix <you@example.com>
2019-12-16 13:41:23 +0100

    step

--- NLP_Basics/spaCy_basics.py
+++ NLP_Basics/spaCy_basics.py
@@ -21,3 +21,30 @@ for token in doc2:
 print(doc2[0])
 print(doc2[0].pos_)
 print(type(doc[0]))
+
+# Spacy Erklärung - GENIAL :)
+print(spacy.explain('mo'))
+
+# weitere Ausgaben
+print(doc2[1].text) # normale Wort
+print(doc2[1].lemma_) # auf welches das Wort zurückzuführen ist
+print(doc2[1].tag_) # Wortart/-verbindung
+print(spacy.explain('VAFIN'))
+print(doc2[1].shape_) # Wortlänge in Form von x´en X-Großbuchstabe x-Kleinbuchstabe
+print(doc2[1].is_alpha) # True wenn alphanummerisch
+print(doc2[1].is_stop) # ob Stoppwort ist
+
+doc3 = nlp_de(u'Der nach John Lennon benannte Flughafen von Liverpool\
+zeigt in der Eingangshalle mehrere großflächige Zitate von Lennon-Songs,\
+darunter Imagine. Auch das zentrale Flughafen-Motto "Above us only sky" ist ein Zitat.')
+
+life_quote = doc3[24:30]
+print(life_quote)
+print(type(life_quote))
+
+doc4 = nlp_de(u'Dies ist der erste Satz. Dies ist ein neuer Satz. Dies ist der letzte Satz.')
+
+for sentence in doc4:
+    print(sentence)
+
+print(doc[4].is_sent_start)
